95afc4ea8f0fa4ae48917ab166a11fac2a8f3ece
QED42 <qed42@SumitMadan.local>
2018-06-15 12:55:22 +0530

    Removed wait sleep

--- src/TestBrowsers.java
+++ src/TestBrowsers.java
@@ -11,13 +11,11 @@ public class TestBrowsers {
 public static void main(String[] args) throws InterruptedException {
 System.setProperty("webdriver.gecko.driver", "/Users/qed42/eclipse/jee-oxygen/Eclipse.app/Contents/MacOS/documents/eclipse-workspace/SeleniumTest/lib/geckodriver/geckodriver");	
 WebDriver driver=new FirefoxDriver();
-
-//Thread.sleep(3000);
+driver.navigate().to("https://www.wikipedia.org/");
 driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 //WebElement element = driver.findElement(By.id("searchLanguage"));
 Select dropdown = new Select(driver.findElement(By.id("searchLanguage")));
 dropdown.selectByValue("az");
-Thread.sleep(3000);
 driver.close();
 	}
 
